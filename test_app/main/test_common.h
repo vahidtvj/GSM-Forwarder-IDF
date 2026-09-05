a79792dcf3baa3671124d968071855bcb1d10302
vahidtvj <vahid.tvj@gmail.com>
2026-09-05 15:29:27 +0330

    sms test 2

--- test_app/main/test_common.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "network_manager.h"
-network_manager_config_t test_default_config(void);
--- test_app/pytest_test_app.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pytest_embedded_idf.dut import IdfDut
-from pytest_embedded_idf.utils import idf_parametrize
-
-
-@pytest.mark.generic
-@idf_parametrize('target', ['esp32'], indirect=['target'])
-def test_all(dut: IdfDut) -> None:
-    dut.run_all_single_board_cases()
